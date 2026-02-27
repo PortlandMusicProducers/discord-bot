3e675cb190d3e5ad8a75421d6d1a7c85b7c8799a
Zachary Burke <zachary.burke@gmail.com>
2026-02-26 17:44:53 -0800

    ..

--- PMPAdmin/PMPAdmin.py
+++ PMPAdmin/PMPAdmin.py
@@ -53,7 +53,7 @@ URL_DISCORD_JOIN = "https://discord.gg/dxBQgPU9Vn"
 #
 UNVERIFIED_HEADER = f"""**🔔Unverified Members Reminder:**
 To become a verified member, please:
-1. Fill out our (welcome form)]{URL_ONBOARDING}].
+1. Fill out our (welcome form)[{URL_ONBOARDING}].
 2. Post an introduction in <#{CHANNEL_ID_INTRO}>.
 3. Mark your genres, tools, and skills in <#{CHANNEL_ID_ROLE_ASSIGNMENT}>
 
